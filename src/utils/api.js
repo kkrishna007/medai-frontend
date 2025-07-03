3de02497085ad17f4d35d86c9ea7382d83fd0402
KKRISHNA SAXENA <saxena.kkrishna@gmail.com>
2025-07-03 15:46:55 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 // src/utils/api.js
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+
 
 export const predictBlindness = async (file) => {
   return uploadImage(file, 'blindness');
